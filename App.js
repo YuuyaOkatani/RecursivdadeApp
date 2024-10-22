373af3a394f78bd7ac97b359fc219590447864ff
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-10-22 02:07:49 -0300

    geometrical stats

--- App.js
+++ App.js
@@ -4,16 +4,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PageOne from './pages/pageOne';
 import PageTwo from './pages/pageTwo';
-
+import Test from './pages/test';
 export default function App() {
 
   const Stack = createStackNavigator(); 
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="PageOne">
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Test">
         <Stack.Screen name= "PageOne" component={PageOne} />
         <Stack.Screen name= "PageTwo" component={PageTwo} />
+        <Stack.Screen name= "Test" component={Test} />
       </Stack.Navigator>
     </NavigationContainer>
   );
